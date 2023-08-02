884d80d7ccad014c8cf5ee09d0c7ae7ecc16e76f
qinzhen123 <42291433+qinzhen123@users.noreply.github.com>
2023-08-02 15:01:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,7 +14,7 @@ const CONFIG = {
   HERO_TITLE_2: '与思维认知',
   HERO_TITLE_3: 'TANGLY1024.COM',
   HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HERO_TITLE_5: 'AI必看',
   HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
@@ -28,7 +28,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是Xzz',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
